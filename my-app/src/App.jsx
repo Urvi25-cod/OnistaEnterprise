9d777bdd93026e889053b6431749c5cf66f50424
Urvi25-cod <hirveurvi46@gmail.com>
2025-04-18 16:50:50 +0530

    18Secondupdate

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,38 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from './Components/Footer';
-import Aboutpage from './Components/Aboutpage';
-import Home from './Components/Home';
-import Contact from './Components/Contact';
-import News from './Components/News';
+// import Footer from './Components/Footer';
+// import Aboutpage from './Components/Aboutpage';
+// import Home from './Components/Home';
+// import Contact from './Components/Contact';
+// import News from './Components/News';
 
-import Cart from './Components/Cart';
-import Singleproduct from './Components/Singleproduct';
-import ProductListing from './Components/ProductListing';
-// import Login from './Components/Login';
-// import Signup from './Components/Signup';
-import Navbar from './Components/Navbar';
-import ProfilePage from "./Components/ProfilePage";
-import ShippingReturns from "./Components/ShippingReturns";
-import PrivacyPolicy from "./Components/PrivacyPolicy";
-import TermsConditions from "./Components/TermsConditions";
-import ProductCustomizer from "./Components/ProductCustomizer";
+// import Cart from './Components/Cart';
+// import Singleproduct from './Components/Singleproduct';
+// import ProductListing from './Components/ProductListing';
+// // import Login from './Components/Login';
+// // import Signup from './Components/Signup';
+// import Navbar from './Components/Navbar';
+// import ProfilePage from "./Components/ProfilePage";
+// import ShippingReturns from "./Components/ShippingReturns";
+// import PrivacyPolicy from "./Components/PrivacyPolicy";
+// import TermsConditions from "./Components/TermsConditions";
 
 
+// import ProductCustomizer from "./Components/ProductCustomizer";
+const ProductCustomizer = lazy(() => import("./Components/ProductCustomizer"));
+const Footer = lazy(() => import("./Components/Footer"));
+const Aboutpage = lazy(() => import("./Components/Aboutpage"));
+const Home = lazy(() => import("./Components/Home"));
+const Contact = lazy(() => import("./Components/Contact"));
+const News = lazy(() => import("./Components/News"));
+const Cart = lazy(() => import("./Components/Cart"));
+const Singleproduct = lazy(() => import("./Components/Singleproduct"));
+const Navbar = lazy(() => import("./Components/Navbar"));
+const ProfilePage = lazy(() => import("./Components/ProfilePage"));
+const ShippingReturns = lazy(() => import("./Components/ShippingReturns"));
+const PrivacyPolicy = lazy(() => import("./Components/PrivacyPolicy"));
+const ProductListing = lazy(() => import("./Components/ProductListing"));
+const TermsConditions = lazy(() => import("./Components/TermsConditions"));
 
 function App() {
 
@@ -28,8 +42,9 @@ function App() {
     <>
 
 
-
+<Suspense fallback={<div>Loading...</div>}>
       <Router>
+
         <Navbar />
         <Routes>
           {/* <Route path='/Login' element={<Login/>} /> */}
@@ -51,7 +66,7 @@ function App() {
         <Footer />
 
       </Router>
-
+      </Suspense>
     </>
   )
 }
